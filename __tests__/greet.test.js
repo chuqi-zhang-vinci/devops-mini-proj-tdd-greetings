f1dd7aa00bfa1854f059941eff1d4b6279972194
karim-naviaux <karim.naviaux@student.vinci.be>
2023-11-14 15:12:22 +0100

    refactor

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -3,3 +3,7 @@ const greet = require('../utils/greet')
 test('Jean-Kevin equals Hello, Jean-Kevin.', () => {
     expect(greet("Jean-Kevin")).toBe("Hello, Jean-Kevin.");
 })
+
+test('should be Jill and Jade.', () => {
+    expect(greet['Jill', 'Jane']).toBe('Hello, Jill and Jane.')
+})
