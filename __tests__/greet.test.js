a4c9d8ccb40cbe8c1c991aa67cd4fbcc46336a29
chuqi-zhang-vinci <chuqi.zhang@student.vinci.be>
2023-11-14 13:24:19 +0100

    test: function greet : add case when the pamarameter is ""

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -3,3 +3,7 @@ const greet = require('../utils/greet')
 test('Jean-Kevin equals Hello, Jean-Kevin.', () => {
     expect(greet('Jean-Kevin')).toBe('Hello, Jean-Kevin.')
 })
+
+test(("'' equals Hello, my friend."), () => {
+    expect(greet("")).toBe("Hello, my friend.")
+})
--- utils/greet.js
+++ utils/greet.js
@@ -1,4 +1,5 @@
 function greet (name) {
+  if (name === '') return 'Hello, my friend.'
   return 'Hello, ' + name + '.'
 }
 
