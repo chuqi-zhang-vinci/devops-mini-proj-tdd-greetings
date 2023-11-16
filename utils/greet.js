48e8019d4ad57343e1ec2b50792e063104a19543
mona-naitmazi <mona.naitmazi@student.vinci.be>
2023-11-16 10:56:08 +0100

    refactor: add getLanguageWords(language) to find the greeting and and words in a separate function

--- utils/greet.js
+++ utils/greet.js
@@ -1,72 +1,72 @@
-function greet (name) {
+function greet(name) {
   if (Array.isArray(name)) {
-    return greetWithArray(name)
+    return greetWithArray(name);
   }
-  if (name === null || name === undefined || name.trim() === '') {
-    return 'Hello, my friend.'
+
+  if (!name || name.trim() === '') {
+    return 'Hello, my friend.';
   }
+
   if (name.toUpperCase() === name) {
-    return 'HELLO, ' + name + '!'
+    return `HELLO, ${name}!`;
   }
-  return 'Hello, ' + name + '.'
+
+  return `Hello, ${name}.`;
 }
 
-function greetWithArray (names) {
-  let language = findLanguage(names)
-  let upperNames = []
-  let lowerNames = []
-  for(const name of names){
-    if(name.toUpperCase() === name && name.toUpperCase() != language){
+function greetWithArray(names) {
+  const language = findLanguage(names);
+  const { greetingWord, andWord } = getLanguageWords(language);
+
+  const upperNames = [];
+  const lowerNames = [];
+
+  for (const name of names) {
+    if (name.toUpperCase() === name && name.toUpperCase() !== language) {
       upperNames.push(name);
-    }else if(name.toUpperCase() != language){
+    } else if (name.toUpperCase() !== language) {
       lowerNames.push(name);
     }
   }
-  const allButLast1 = lowerNames.slice(0, -1).join(', ')
-  const last1 = lowerNames[lowerNames.length - 1]
 
-  let greetingWord, andWord; 
+  const allButLast = lowerNames.slice(0, -1).join(', ');
+  const last = lowerNames[lowerNames.length - 1];
 
-  switch(language){
-    case "FR" :
-      greetingWord = "Bonjour";
-      andWord = "et";
-      break;
-    
-    case "NL" :
-      greetingWord = "Goeiedag";
-      andWord = "en";
-      break; 
-    
-    default :
-      greetingWord = "Hello";
-      andWord = "and";
-  }
-
-  if(upperNames.length === 1){
-    return `${greetingWord}, ${allButLast1} ${andWord} ${last1}. ${andWord.toUpperCase()} ${greetingWord.toUpperCase()} ${upperNames[0]}!`
-  }else if(upperNames.length === 0){
-    return `${greetingWord}, ${allButLast1} ${andWord} ${last1}.`
-  }else{
-    const allButLast2 = upperNames.slice(0, -1).join(', ')
-    const last2 = upperNames[upperNames.length - 1]
-    return `${greetingWord}, ${allButLast1} ${andWord} ${last1}. ${andWord.toUpperCase()} ${greetingWord.toUpperCase()} ${allButLast2} ${andWord.toUpperCase()} ${last2}!`
+  if (upperNames.length === 1) {
+    return `${greetingWord}, ${allButLast} ${andWord} ${last}. ${andWord.toUpperCase()} ${greetingWord.toUpperCase()} ${upperNames[0]}!`;
+  } else if (upperNames.length === 0) {
+    return `${greetingWord}, ${allButLast} ${andWord} ${last}.`;
+  } else {
+    const allButLastUpper = upperNames.slice(0, -1).join(', ');
+    const lastUpper = upperNames[upperNames.length - 1];
+    return `${greetingWord}, ${allButLast} ${andWord} ${last}. ${andWord.toUpperCase()} ${greetingWord.toUpperCase()} ${allButLastUpper} ${andWord.toUpperCase()} ${lastUpper}!`;
   }
 }
 
-function findLanguage(array){
-  for(const element of array){
-    if(element.toUpperCase() === "FR"){
-      return "FR"
-    }else if(element.toUpperCase() === "NL"){
-      return "NL"
-    }else if(element.toUpperCase === "EN"){
-      return "EN"
+function findLanguage(array) {
+  for (const element of array) {
+    const upperElement = element.toUpperCase();
+    if (upperElement === "FR") {
+      return "FR";
+    } else if (upperElement === "NL") {
+      return "NL";
+    } else if (upperElement === "EN") {
+      return "EN";
     }
   }
 
-  return "EN"
+  return "EN";
 }
 
+function getLanguageWords(language) {
+  switch (language) {
+    case "FR":
+      return { greetingWord: "Bonjour", andWord: "et" };
+    case "NL":
+      return { greetingWord: "Goeiedag", andWord: "en" };
+    default:
+      return { greetingWord: "Hello", andWord: "and" };
+  }
+}
 
-module.exports = greet
+module.exports = greet;
